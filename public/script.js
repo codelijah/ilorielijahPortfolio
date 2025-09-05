ad6b300754f5d8d0882296977a1b2b52aa34500e
codelijah <143726005+codelijah@users.noreply.github.com>
2025-09-05 19:44:09 +0100

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,37 @@
+const themeToggleBtn = document.getElementById('themeToggleBtn');
+const html = document.documentElement;
+const menuBtn = document.getElementById('menu-btn');
+const navDropDown = document.querySelector('.nav-dropdown');
+const dropdownContent = document.getElementById('dropdownContent');
+
+themeToggleBtn.addEventListener('click', () => {
+  html.classList.toggle('dark');
+  // Optionally, save theme preference
+  // if (html.classList.contains('dark')) {
+  //   localStorage.setItem('theme', 'dark');
+  // } else {
+  //   localStorage.setItem('theme', 'light');
+  // }
+});
+
+// if (localStorage.getItem('theme') === 'dark') {
+//   html.classList.add('dark');
+// } else {
+//   html.classList.remove('dark');
+// }
+
+html.classList.add('dark');
+
+
+
+menuBtn.addEventListener('click', function () {
+  navDropDown.classList.toggle('opacity-0');
+  navDropDown.classList.toggle('opacity-100');
+  navDropDown.classList.toggle('pointer-events-none');
+  navDropDown.classList.toggle('pointer-events-auto');
+  menuBtn.classList.toggle('active');
+  dropdownContent.classList.toggle('opacity-0');
+  dropdownContent.classList.toggle('opacity-100');
+  dropdownContent.classList.toggle('-translate-y-60');
+  dropdownContent.classList.toggle('translate-y-0');
+});
---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.html'],
+  darkMode: 'class',
+  theme: {
+    screens: {
+      sm: '390px',
+      md: '768px',
+      lg: '1024px',
+      // xl: "1440px",
+      // "2xl": "1920px",
+    },
+    extend: {
+      fontFamily: {
+        inter: ['inter', 'sans-serif'],
+      },
+      keyframes: {
+        'fade-in': {
+          '0%': { opacity: '0', transform: 'translateY(20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        'fade-in': 'fade-in 0.6s ease-out',
+      },
+
+      colors: {
+        charcoal: 'rgb(26,26,26)',
+        lightGrey: 'rgb(229, 231, 235)',
+        lightGreyDark: 'rgb(121, 123, 127)',
+        greyishBlue: 'hsl(215, 16%, 65%)',
+        dark: '#000000',
+        light: '#FFFFFF',
+        borderGrey: 'hsl(0, 0%, 25%)',
+        cardBgGrey: 'hsl(0, 0%, 14%)',
+        darkGrey: 'hsl(0, 0%, 14%)',
+        cardBodyGrey: 'hsl(0, 0%, 70%)',
+        cardHeadingGrey: 'hsl(0, 0%, 100%)',
+        // cardLoad: 'hsl(217, 91%, 60%)',
+        languageBg: 'hsl(220, 13%, 15%)',
+        languageBorder:'hsl(0, 0%, 20%)'
+      },
+    },
+  },
+  plugins: [],
+};

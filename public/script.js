3ad05681e27d5b2988a4794a549b09d0b3372791
codelijah <143726005+codelijah@users.noreply.github.com>
2025-09-13 02:21:29 +0100

    updated

--- public/script.js
+++ public/script.js
@@ -3,8 +3,9 @@ const html = document.documentElement;
 const menuBtn = document.getElementById('menu-btn');
 const navDropDown = document.querySelector('.nav-dropdown');
 const dropdownContent = document.getElementById('dropdownContent');
-const nameDropdown = document.getElementById('namedropdown');
+const nameSlide = document.getElementById('name-slide');
 const stylishNameDropdown = document.getElementById('stylishName');
+const headerText = document.getElementById('header-text');
 
 themeToggleBtn.addEventListener('click', () => {
   html.classList.toggle('dark');
@@ -25,14 +26,33 @@ themeToggleBtn.addEventListener('click', () => {
 html.classList.add('dark');
 
 menuBtn.addEventListener('click', function () {
+  // Toggle dropdown visibility
   navDropDown.classList.toggle('opacity-0');
+  navDropDown.classList.toggle('opacity-100');
   navDropDown.classList.toggle('pointer-events-none');
+  navDropDown.classList.toggle('pointer-events-auto');
+  headerText.classList.toggle('backdrop-blur-md');
+
+  // Toggle hamburger animation (optional)
   menuBtn.classList.toggle('active');
-  document.body.classList.toggle('overflow-y-hidden');
+
+  // Animate content
   dropdownContent.classList.toggle('opacity-0');
+  dropdownContent.classList.toggle('opacity-100');
   dropdownContent.classList.toggle('-translate-y-60');
-  nameDropdown.classList.toggle('opacity-0');
-  nameDropdown.classList.toggle('-translate-y-40');
+  dropdownContent.classList.toggle('translate-y-0');
+
   stylishNameDropdown.classList.toggle('opacity-0');
+  stylishNameDropdown.classList.toggle('opacity-100');
   stylishNameDropdown.classList.toggle('-translate-y-72');
+  stylishNameDropdown.classList.toggle('translate-y-0');
+
+  nameSlide.classList.toggle('translate-x-10');
+  menuBtn.classList.toggle('-translate-x-10');
+  // Lock / Unlock scroll for mobile
+  if (document.body.style.overflow === 'hidden') {
+    document.body.style.overflow = '';
+  } else {
+    document.body.style.overflow = 'hidden';
+  }
 });
